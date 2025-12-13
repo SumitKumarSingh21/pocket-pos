d9494f34a39df3331118bb1299288072cb05a3eb
Sumit Kumar Singh <117089367+SumitKumarSingh21@users.noreply.github.com>
2025-12-14 00:48:11 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import Index from './pages';
 
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element (#root) not found');
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Index />
+    </HashRouter>
+  </React.StrictMode>
+);
